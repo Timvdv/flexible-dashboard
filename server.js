70f41fc0820614c801264b3eb1fc320889a005cc
Tim van de Vathorst <tim@vandevathorst.nl>
2016-10-14 21:02:55 +0200

    run production script

--- server.js
+++ server.js
@@ -1,24 +1,48 @@
 var webpack = require('webpack'),
     WebpackDevServer = require('webpack-dev-server'),
     config = require('./webpack.config'),
-    path = require("path");
+    path = require("path"),
+    express = require('express'),
+    app = express(),
+    dev = false;
 
-var server = new WebpackDevServer(webpack(config), {
-    publicPath: config.output.publicPath,
-    headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
-    }
+process.argv.forEach(function (val, index, array) {
+  if(val == "--development")
+  {
+    dev = true;
+  }
 });
 
-server.use('/', function(req, res) {
-    res.sendFile(path.join(__dirname+'/index.html'));
-});
+if(dev)
+{
+    var server = new WebpackDevServer(webpack(config), {
+        publicPath: config.output.publicPath,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+        }
+    });
 
-server.listen(3003, 'localhost', function (err, result) {
-    if (err) {
-      console.log(err);
-    }
+    server.use('/', function(req, res) {
+        res.sendFile(path.join(__dirname+'/index.html'));
+    });
 
-    console.log('Running at http://localhost:3003');
-});
+    server.listen(3003, 'localhost', function (err, result) {
+        if (err) {
+          console.log(err);
+        }
+
+        console.log('Running at http://localhost:3003');
+    });    
+}
+else
+{
+    app.use('/', express.static('dist'));
+    app.get('/', function (req, res) {
+        res.sendFile(path.join(__dirname + '/index.html'));
+    });
+
+    app.listen(3003, function () {
+      console.log('Running at http://localhost:3003');
+    });   
+}
